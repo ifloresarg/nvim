75b59bc5c32871d164b1ac136c65f3dd853df7e0
Ignacio Flores <ignacio.simon.flores@gmail.com>
2026-02-26 06:46:01 -0300

    feat: add initial project structure including configuration and necessary files

---
+++ lua/plugins/overrides.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "storm",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
