3a2ebeb0f80ff370f75e03f05af36786563e1c28
Ignacio Flores <ignacio.simon.flores@gmail.com>
2026-02-27 12:18:09 -0300

    fix: update SchemaStore.nvim and copilot.lua commits in lazy-lock.json

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -4,6 +4,26 @@ return {
     opts = {
       inlay_hints = { enabled = false },
     },
+    init = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client and client.server_capabilities.signatureHelpProvider then
+            client.server_capabilities.signatureHelpProvider.triggerCharacters = {}
+          end
+        end,
+      })
+    end,
+    keys = {
+      {
+        "<C-k>",
+        function()
+          vim.lsp.buf.signature_help()
+        end,
+        mode = "i",
+        desc = "Signature Help",
+      },
+    },
   },
   {
     "folke/tokyonight.nvim",
@@ -74,6 +94,25 @@ return {
             },
           },
         },
+      },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        trigger = {
+          show_on_keyword = false,
+          show_on_trigger_character = false,
+          show_on_insert_on_trigger_character = false,
+          show_on_accept_on_trigger_character = false,
+        },
+        menu = {
+          auto_show = false,
+        },
+      },
+      keymap = {
+        ["<A-Space>"] = { "show", "hide" },
       },
     },
   },
